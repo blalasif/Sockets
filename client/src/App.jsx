dfcba05f4882e09c0e5a77fa71af95203c49b8d6
Bilal <bilaljee684@gmail.com>
2025-04-28 02:18:17 -0700

    chat-system done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,33 @@
 import React, { useEffect, useMemo, useState } from 'react'
 import {io} from "socket.io-client"
-import {Button, Container, TextField, Typography} from "@mui/material"
+import {Box, Button, Container, Stack, TextField, Typography} from "@mui/material"
 
 const App = () => {
+  const [messages, setMessages] = useState([]);
 const [message,setMessage] = useState('')
 const [room,setRoom] = useState('')
 const[socketid,setSocketId] = useState('')
+const[roomName,setRoomName] = useState('')
 
-  const socket = useMemo(()=>io("http://localhost:2000"),[])
+  const socket = useMemo(()=>io("http://localhost:2000",{
+    withCredentials:true,
+  }),[])
 
 
+
+
+
+  const joinRoomHandler = (e)=>{
+    e.preventDefault();
+    socket.emit('join-room',roomName)
+    setRoomName("")
+  }
   useEffect(()=>{
   socket.on("connect",()=>{
     setSocketId(socket.id)
   socket.on("recieve-message",(data)=>{
     console.log("🚀 ~ socket.on ~ data:", data)
-    
+  setMessages((prev) => [...prev, data]);     
 
   });
   socket.on("welcome",(s)=>{
@@ -38,28 +50,61 @@ const[socketid,setSocketId] = useState('')
    };
  
   return (
-    <> 
-    
-    <Container maxWidth="sm" >
-      {/* <Typography variant= "h1" component="div" gutterBottom>
+    <>
+      <Container maxWidth="sm">
+        <Box sx={{ height: "200px" }}>
+          {/* <Typography variant= "h1" component="div" gutterBottom>
         Welcome to Socket.IO
       </Typography> */}
-      <Typography variant= "h6" component="div" gutterBottom>
-        {socketid}
-      </Typography>
+          <Typography variant="h6" component="div" gutterBottom>
+            {socketid}
+          </Typography>
 
-      <form action="" onSubmit={handleSubmit}>
-        <TextField value={message} name='message' onChange={e=>setMessage(e.target.value)} id='outlined-basic' label="Message" variant="outlined"/>
-        <TextField value={room} name='room' onChange={e=>setRoom(e.target.value)} id='outlined-basic' label="Room" variant="outlined"/>
-        <Button type='submit' variant='contained' color='primary'>Send</Button>
-      </form>
+          <form action="" onSubmit={joinRoomHandler}>
+            <h5>Join Room</h5>
+            <TextField
+              value={roomName}
+              name="message"
+              onChange={(e) => setRoomName(e.target.value)}
+              id="outlined-basic"
+              label="Room Name"
+              variant="outlined"
+            />{" "}
+            <Button type="submit" variant="contained" color="primary">
+              Join
+            </Button>
+          </form>
 
-    </Container>
-    
-    
+          <form action="" onSubmit={handleSubmit}>
+            <TextField
+              value={message}
+              name="message"
+              onChange={(e) => setMessage(e.target.value)}
+              id="outlined-basic"
+              label="Message"
+              variant="outlined"
+            />
+            <TextField
+              value={room}
+              name="room"
+              onChange={(e) => setRoom(e.target.value)}
+              id="outlined-basic"
+              label="Room"
+              variant="outlined"
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Send
+            </Button>
+          </form>
+          <Stack>
+            {messages.map((m, i) => (
+              <Typography key={i}>{m}</Typography>
+            ))}
+          </Stack>
+        </Box>
+      </Container>
     </>
-
-  )
+  );
 }
 
 export default App
--- server/app.js
+++ server/app.js
@@ -2,19 +2,50 @@ import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
 import cors from 'cors'
+import jwt from 'jsonwebtoken'
+import cookieParser from "cookie-parser";
+
+
 const app = express();
-app.use(cors())
+app.use(
+  cors({
+    origin: "http://localhost:5173", // 👈 again, not "*"
+    credentials: true,
+  })
+);
 //socket server
 const server = new createServer(app);
 // io instance
-const io = new Server(server,{
-    cors:{
-        origin:"*",
-        methods:['GET','POST'],
-        credentials:true
-    }
+const secret_key = "sdfaaaaaaa";
+
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // 👈 your frontend URL, NOT "*"
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
 
+
+app.get("/login",(req,res)=>{
+  const token = jwt.sign({ _id: "jlkfdaaaaaaaaaa" }, secret_key);
+  res.cookie("token",token,{httpOnly:true,secure:true,sameSite:'none'}).json({
+    message:"Login Success"
+  })
+})
+
+const user = false
+io.use((socket, next) => {
+  cookieParser()(socket.request,socket.request.res,(err)=>{
+    if(err) return next(err)
+      const token = socket.request.cookies.token;
+    if(!token) return next(new Error("Authentication Error"))
+      const decoded = jwt.verify(token,secret_key)
+    next()
+
+  })
+});
 io.on("connection", (socket) => {
   console.log("🚀 ~ io.on ~ socket.id:", socket.id);
   // socket.emit("welcome","Welcome to our Chat-App")
@@ -25,6 +56,12 @@ io.on("connection", (socket) => {
   io.to(room).emit("recieve-message", message);
 
   })
+
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log(`🚀 ~User Join the room ~ room ${room}`)
+    
+  });
   socket.on("disconnect",()=>{
     console.log("User Disconnected",socket.id)
   })
